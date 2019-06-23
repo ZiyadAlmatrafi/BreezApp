d31a08a414f4fb2ed2bcbfd3ccd9aaf26819db8a
ZiyadAlmatrafi <ziad1378@gmail.com>
2019-06-23 20:57:27 +0300

    Added Launcher logo

--- app/src/main/java/com/example/breezapp/MainActivity.java
+++ app/src/main/java/com/example/breezapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.breezapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
     }
 }
