85c8ef735e8a18983e8ec714d6c21c5691edc8e1
unknown <kalthommusa12345@gmail.com>
2019-06-24 17:04:53 +0300

    fragments first trial

--- app/src/main/java/com/example/breezapp/HomeActivity.java
+++ app/src/main/java/com/example/breezapp/HomeActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class HomeActivity extends AppCompatActivity {
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHubActivity();
+            }
+        });
+    }
+
+    public void openHubActivity() {
+        Intent intent = new Intent(this, HubActivity.class);
+        startActivity(intent);
     }
 }
+
