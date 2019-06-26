cdc006f873017a17e3667ff52a0dca60c5220b2b
ZiyadAlmatrafi <ziad1378@gmail.com>
2019-06-26 14:55:22 +0300

    Added try again button in HubNotFoundFragment, Removed top action bar from all UI

--- app/src/main/java/com/example/breezapp/Fragments/HubNotFoundFragment.java
+++ app/src/main/java/com/example/breezapp/Fragments/HubNotFoundFragment.java
@@ -5,16 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.breezapp.R;
 
 
 public class HubNotFoundFragment extends Fragment {
-
+    Button notFound;
 
 
     @Override
@@ -27,8 +29,21 @@ public class HubNotFoundFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hub_not_found, container, false);
+        View v = inflater.inflate(R.layout.fragment_hub_not_found, container, false);
+
+        notFound = (Button) v.findViewById(R.id.notFoundButton);
+        notFound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                ft.replace(R.id.frame, new ConnectedHubFragment());
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        return v;
+
+
     }
-
-
 }
