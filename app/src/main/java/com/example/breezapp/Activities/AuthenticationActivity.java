8ad59e179157f481dbe11b0983e76219cab16974
Nuhaa Alshawi <nuhaa95@hotmail.com>
2019-06-27 11:35:43 +0300

    Add network connection operation for application

--- app/src/main/java/com/example/breezapp/Activities/AuthenticationActivity.java
+++ app/src/main/java/com/example/breezapp/Activities/AuthenticationActivity.java
@@ -14,6 +14,14 @@ import com.example.breezapp.Fragments.ElectricityFragment;
 import com.example.breezapp.Fragments.SignInFragment;
 import com.example.breezapp.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AuthenticationActivity extends AppCompatActivity {
         Button button;
     @Override
@@ -24,6 +32,45 @@ public class AuthenticationActivity extends AppCompatActivity {
         setFragment(new SignInFragment());
     }
 
+    /* private void JasonParsa(){
+
+    HttpURLConnection connection = null;
+    BufferedReader reader = null;
+
+        try {
+        URL url = new URL ("");
+        connection = (HttpURLConnection).url.openConnection();
+        connection.connect();
+
+        InputStream stream = connection.getInputStream();
+
+        reader = new BufferedReader(new InputStreamReader(stream));
+
+        StringBuffer buffer = new StringBuffer();
+
+        String line="";
+        while ((line = reader.readLine())!= null){
+            buffer.append(line);
+        }
+
+    }catch (MalformedURLException e){
+        e.printStackTrace();
+    }catch (IOException e){
+        e.printStackTrace();
+    }finally {
+        if(connection != null){
+            connection.disconnect();
+        }
+        try {
+            if(reader != null){
+                reader.close();}
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+*/
     public void setFragment(Fragment f){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
