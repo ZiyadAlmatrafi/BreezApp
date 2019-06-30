b2c0cd2f344a9bd99827c268f99a0096d1a49e13
ZiyadAlmatrafi <ziad1378@gmail.com>
2019-06-30 13:30:39 +0300

    Save changes

---
+++ app/src/main/java/com/example/breezapp/Models/Setting.java
@@ -0,0 +1,14 @@
+package com.example.breezapp.Models;
+
+public class Setting {
+
+    public String option;
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+}
