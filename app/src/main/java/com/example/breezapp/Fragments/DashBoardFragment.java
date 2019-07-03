1ed6949cace7a6b82f79dfe279fa203055f26b52
ZiyadAlmatrafi <ziad1378@gmail.com>
2019-07-03 16:57:21 +0300

    Added RecyclerView in Dashboard fragment

--- app/src/main/java/com/example/breezapp/Fragments/DashBoardFragment.java
+++ app/src/main/java/com/example/breezapp/Fragments/DashBoardFragment.java
@@ -5,21 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.breezapp.Adapters.DashBoardAdapter;
+import com.example.breezapp.Models.Device;
 import com.example.breezapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DashBoardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DashBoardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DashBoardFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,7 +38,37 @@ public class DashBoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+        View v = inflater.inflate(R.layout.fragment_dash_board, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView_dash);
+
+        Device d1 = new Device("Lamp",R.drawable.lamp);
+        Device d2 = new Device("light bulb",R.drawable.light_bulb);
+        Device d3 = new Device("Wall lamp",R.drawable.wall_lamp);
+        Device d4 = new Device("Lamp",R.drawable.lamp);
+        Device d5 = new Device("light bulb",R.drawable.light_bulb);
+        Device d6 = new Device("Wall lamp",R.drawable.wall_lamp);
+        Device d7 = new Device("Lamp",R.drawable.lamp);
+        Device d8 = new Device("light bulb",R.drawable.light_bulb);
+        Device d9 = new Device("Wall lamp",R.drawable.wall_lamp);
+
+        Device device[] = {new Device(d1.getName(),d1.getImg()),
+                new Device(d2.getName(),d2.getImg()),
+                new Device(d3.getName(),d3.getImg()),
+                new Device(d4.getName(),d4.getImg()),
+                new Device(d5.getName(),d5.getImg()),
+                new Device(d6.getName(),d6.getImg()),
+                new Device(d7.getName(),d7.getImg()),
+                new Device(d8.getName(),d8.getImg()),
+                new Device(d9.getName(),d9.getImg()),
+
+        };
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        DashBoardAdapter mAdapter = new DashBoardAdapter(device,getContext());
+
+        recyclerView.setAdapter(mAdapter);
+
+        return v;
     }
 
 
