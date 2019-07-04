f0ab4b5a450c1aaa7bc26352ca7e877881956d1f
ZiyadAlmatrafi <ziad1378@gmail.com>
2019-07-04 12:39:44 +0300

    merge

--- app/src/main/java/com/example/breezapp/fragments/RoutineFragment.java
+++ app/src/main/java/com/example/breezapp/fragments/RoutineFragment.java
@@ -1,42 +1,60 @@
 package com.example.breezapp.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.breezapp.adapters.RoutinesAdapter;
+import com.example.breezapp.models.Routines;
 import com.example.breezapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RoutineFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RoutineFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RoutineFragment extends Fragment {
+
+import java.util.ArrayList;
 
 
-    public RoutineFragment() {
-        // Required empty public constructor
-    }
+public class RoutineFragment extends Fragment  {
+
+    TextView textView;
+    private static final String TAG = RoutineFragment.class.getSimpleName();
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<Routines> arrayList = new ArrayList<>();
+
+    int [] im_id={R.drawable.morning,R.drawable.night};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routine, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+
+
+
+        View view = inflater.inflate(R.layout.fragment_routine, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        layoutManager = (new GridLayoutManager(getContext(),2));
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+
+        for (int id : im_id){
+
+            arrayList.add(new Routines(id));
+
+        }
+
+
+        adapter= new RoutinesAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 
