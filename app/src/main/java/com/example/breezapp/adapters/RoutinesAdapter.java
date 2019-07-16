e5599bc8260c84dc40580b39195c01f7fb787b59
unknown <kalthommusa12345@gmail.com>
2019-07-16 11:19:12 +0300

    Change Routine fragment's icons

--- app/src/main/java/com/example/breezapp/adapters/RoutinesAdapter.java
+++ app/src/main/java/com/example/breezapp/adapters/RoutinesAdapter.java
@@ -1,6 +1,4 @@
 package com.example.breezapp.adapters;
-
-
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -8,25 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.breezapp.activities.RoutinesActivity;
 import com.example.breezapp.models.Routines;
 import com.example.breezapp.R;
-
-
-
 import java.util.ArrayList;
-
-
 public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.MyViewHolder>  {
-
 
     private Context context;
     private ArrayList<Routines>  routines;
-
 
     public RoutinesAdapter(ArrayList<Routines> routines, Context context) {
         this.context=context;
@@ -35,48 +26,46 @@ public class RoutinesAdapter extends RecyclerView.Adapter<RoutinesAdapter.MyView
 
     @NonNull
     @Override
-    public RoutinesAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.routines_row_layout, null);
         return new MyViewHolder(view);
-
     }
 
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            // position is The position of a data item within an Adapter.
-            holder.mImageView.setImageResource(routines.get(position).getImg_id());
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
 
-        }
-
-
-        @Override
-        public int getItemCount() {
-
-            return routines.size();
-        }
-
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-
-            public ImageView mImageView;
-
-
-            public MyViewHolder(View itemView) {
-                super(itemView);
-
-                mImageView = itemView.findViewById(R.id.routines);
-                mImageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        Log.e("routineadapter","inside on click");
-
-                        Intent intent = new Intent(context, RoutinesActivity.class);
-                        intent.putExtra("option", mImageView.getId());
-                        context.startActivity(intent);
-                    }
-                });
-            }
-        }
+        holder.mImageView.setImageResource(routines.get(position).getImg_id());
     }
 
+    @Override
+    public int getItemCount() {
+        return routines.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView mImageView;
+        public TextView routineType;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.routines);
+            mImageView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+
+                    Log.e("routineadapter","inside on click");
+                    Intent intent = new Intent(context, RoutinesActivity.class);
+                    intent.putExtra("option", mImageView.getId());
+                    Log.e("routineadapter","is"+ mImageView.getId());
+                    intent.putExtra("position", getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+}
 
