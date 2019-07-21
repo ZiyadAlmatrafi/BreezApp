61ca01a614c3300749597d3d14539e6e277d25a7
unknown <kalthommusa12345@gmail.com>
2019-07-21 10:19:56 +0300

    Make buttons rounded

--- app/src/main/java/com/example/breezapp/activities/RoutinesActivity.java
+++ app/src/main/java/com/example/breezapp/activities/RoutinesActivity.java
@@ -3,7 +3,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.breezapp.R;
-import com.example.breezapp.fragments.NightFragment;
 import com.example.breezapp.fragments.RoutineEventFragment;
 
 public class RoutinesActivity extends AppCompatActivity {
--- app/src/main/java/com/example/breezapp/fragments/HubFragment.java
+++ app/src/main/java/com/example/breezapp/fragments/HubFragment.java
@@ -1,11 +1,14 @@
 package com.example.breezapp.fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +20,9 @@ import com.example.breezapp.activities.HomeActivity;
 import com.example.breezapp.activities.MapsActivity;
 import com.example.breezapp.R;
 
+import static android.content.Context.MODE_PRIVATE;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +32,7 @@ public class HubFragment extends Fragment {
 
 
     Button hub_button;
-    TextView use_maps;
+    TextView use_maps , street , locality , city_state;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,22 @@ public class HubFragment extends Fragment {
         hub_button = (Button) v.findViewById(R.id.hub_button);
         use_maps = (TextView) v.findViewById(R.id.use_maps);
 
+        street = (TextView) v.findViewById(R.id.street);
+        locality = (TextView) v.findViewById(R.id.locality);
+        city_state = (TextView) v.findViewById(R.id.city_state);
+
+        SharedPreferences preferences = getActivity().getSharedPreferences("MY_SHARED_PREF", MODE_PRIVATE);
+
+        String the_address = preferences.getString("the_address" , "");
+        String the_city = preferences.getString("the_city" , "");
+        String the_state = preferences.getString("the_state" , "");
+        String the_country = preferences.getString("the_country" , "");
+        String the_postalCode = preferences.getString("the_postalCode" , "");
+        String the_knownName = preferences.getString("the_knownName" , "");
+
+        street.setText(the_address);
+        locality.setText(the_country + ", " + the_postalCode + ", " + the_knownName);
+        city_state.setText(the_city +", " + the_state);
 
         hub_button.setOnClickListener(new View.OnClickListener() {
             @Override
